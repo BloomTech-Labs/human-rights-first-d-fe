0591c3ad771f9a826776aadcd016cacc4cd8a30a
Heath Scott <you@example.com>
2020-11-13 14:33:46 -0600

    🧹 clean up code

--- src/components/Visualization.js
+++ src/components/Visualization.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import Plot from 'react-plotly.js';
 import { useSelector, useDispatch } from 'react-redux';
-import { mapSelection } from '../reducers/mapActions';
+import { mapSelection } from '../state/actions/mapActions';
 // import { ADD_MAP, mapReducer } from '../reducers/map_reducer';
 // import { mapSelection } from '../reducers/mapActions';
 
@@ -12,11 +12,10 @@ export default function Visualization() {
   const mapLayout = useSelector(state => state.map.layout);
 
   const dispatch = useDispatch();
-  console.log('visual', mapData);
 
   useEffect(() => {
     dispatch(mapSelection());
-    console.log('map select', mapData);
+    // console.log('map select', mapData);
   }, []);
 
   //Todo: make map full width and height
