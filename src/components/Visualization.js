2a6cf8744ce64ecd1d7389402da6fcc7a7c837cb
Bikesh <Biku@Bikeshs-MacBook-Air-2.local>
2020-11-13 14:55:51 -0800

    updated the components

--- src/components/Visualization.js
+++ src/components/Visualization.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Plot from 'react-plotly.js';
+import { useSelector, useDispatch } from 'react-redux';
+import { barSelection } from '../state/reducers/barActions';
 
-const Visualization = () => {
-  return <h1>Visualization</h1>;
-};
-export default Visualization;
+export default function Visualization() {
+  //states
+  const barData = useSelector(state => state.bar.data);
+  const barLayout = useSelector(state => state.bar.layout);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(barSelection());
+  }, []);
+
+  //Todo: make map full width and height
+  return (
+    <div>
+      <Plot data={barData} layout={barLayout} />
+    </div>
+  );
+}
--- src/components/forms/BarGraph.js
+++ src/components/forms/BarGraph.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
-import { Radio, DatePicker, Button, Form, Select, Checkbox } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Radio, DatePicker, Button, Form, Input } from 'antd';
 import moment from 'moment';
-import axios from 'axios';
 
-import { ADD_BAR_GRAPH } from '../../state/reducers/graph_reducers';
-import { useDispatch } from 'react-redux';
+import Plot from 'react-plotly.js';
+import { barSelection } from '../../state/reducers/barActions';
+
+import { useSelector, useDispatch } from 'react-redux';
 
 const { RangePicker } = DatePicker;
 
@@ -48,6 +49,15 @@ export default function BarGraph() {
   const dispatch = useDispatch();
   const [state, setState] = useState(defaultFilterState);
 
+  const barData = useSelector(state => state.bar.data);
+
+  const barLayout = useSelector(state => state.bar.layout);
+
+  // helper functions
+  useEffect(() => {
+    dispatch(barSelection());
+  }, []);
+
   // FORM ACTIONS
 
   const layout = {
@@ -58,12 +68,7 @@ export default function BarGraph() {
       span: 16,
     },
   };
-  // const tailLayout = {
-  //   wrapperCol: {
-  //     offset: 8,
-  //     span: 16,
-  //   },
-  // };
+
   const onChange = event => {
     setState({
       ...state,
@@ -71,13 +76,13 @@ export default function BarGraph() {
     });
     console.log(state);
   };
-  const onRadioChange = e => {
-    console.log('radio checked', e.target.value);
-    const { name, value } = e.target;
-    setState({
-      [name]: value,
-    });
-  };
+  // const onRadioChange = e => {
+  //   console.log('radio checked', e.target.value);
+  //   const { name, value } = e.target;
+  //   setState({
+  //     [name]: value,
+  //   });
+  // };
   const handleDemographic = () => {
     setState(prev => ({
       ...prev,
@@ -92,18 +97,6 @@ export default function BarGraph() {
     }));
   };
 
-  const onFinish = async values => {
-    // get plotly data from the backedn server
-    let bar_graph = await axios.post(
-      'https://hrf-d-api.herokuapp.com/ds_server/us_bar',
-      { user_input: values.select_state }
-    );
-
-    bar_graph = JSON.parse(bar_graph.data.unemloyment_rate);
-
-    dispatch({ type: ADD_BAR_GRAPH, payload: bar_graph });
-  };
-
   return (
     <Form
       {...layout}
@@ -111,8 +104,9 @@ export default function BarGraph() {
       initialValues={{
         remember: true,
       }}
-      onFinish={onFinish}
     >
+      <Plot data={barData.data} layout={barLayout.layout} />
+
       {/* Start date and End Date */}
       <RangePicker
         defaultValue={[moment(state.start_date), moment(state.end_date)]}
@@ -132,6 +126,7 @@ export default function BarGraph() {
       {/*  Ascending true or false */}
       {/*  Title: Order By */}
       {/*  Most reports and Least reports  */}
+
       <Radio.Group
         options={ascOptions}
         onChange={onChange}
@@ -155,9 +150,13 @@ export default function BarGraph() {
       />
       {/* Title: Focus on   */}
 
+      <Input placeholder=" Select State" />
+      <Input placeholder="City" />
+      <Input placeholder="Zipcode" />
+
       {/*  Submit and the reset buttons */}
       <Button
-        onClick={onFinish}
+        // onClick={onFinish}
         style={{ marginTop: '20px' }}
         type="primary"
         shape="round"
@@ -178,44 +177,44 @@ export default function BarGraph() {
   );
 }
 
-const Demographic = ({
-  showDemographic,
-  setState,
-  demographic,
-  addDemographic,
-}) => {
-  return (
-    <div style={{ paddingLeft: 0, marginBottom: 20, marginTop: 20 }}>
-      <div style={{ marginBottom: 10 }}>
-        <Checkbox onChange={setState}>Demographic</Checkbox>
-      </div>
-      {showDemographic && (
-        <Select
-          mode="multiple"
-          style={{ width: '100%' }}
-          placeholder="select one"
-          defaultValue={demographic}
-          onChange={addDemographic}
-          optionLabelProp="label"
-        >
-          <Select.Option value="black" label="Black">
-            <div className="demo-option-label-item">Black</div>
-          </Select.Option>
-          <Select.Option value="white" label="White">
-            <div className="demo-option-label-item">White</div>
-          </Select.Option>
-          <Select.Option value="asian" label="Asian">
-            <div className="demo-option-label-item">Asian</div>
-          </Select.Option>
-          <Select.Option value="pacific islander" label="Pacific Islander">
-            <div className="demo-option-label-item">Pacific Islander</div>
-          </Select.Option>
+// const Demographic = ({
+//   showDemographic,
+//   setState,
+//   demographic,
+//   addDemographic,
+// }) => {
+//   return (
+//     <div style={{ paddingLeft: 0, marginBottom: 20, marginTop: 20 }}>
+//       <div style={{ marginBottom: 10 }}>
+//         <Checkbox onChange={setState}>Demographic</Checkbox>
+//       </div>
+//       {showDemographic && (
+//         <Select
+//           mode="multiple"
+//           style={{ width: '100%' }}
+//           placeholder="select one"
+//           defaultValue={demographic}
+//           onChange={addDemographic}
+//           optionLabelProp="label"
+//         >
+//           <Select.Option value="black" label="Black">
+//             <div className="demo-option-label-item">Black</div>
+//           </Select.Option>
+//           <Select.Option value="white" label="White">
+//             <div className="demo-option-label-item">White</div>
+//           </Select.Option>
+//           <Select.Option value="asian" label="Asian">
+//             <div className="demo-option-label-item">Asian</div>
+//           </Select.Option>
+//           <Select.Option value="pacific islander" label="Pacific Islander">
+//             <div className="demo-option-label-item">Pacific Islander</div>
+//           </Select.Option>
 
-          <Select.Option value="other" label="Other">
-            <div className="demo-option-label-item">Other</div>
-          </Select.Option>
-        </Select>
-      )}
-    </div>
-  );
-};
+//           <Select.Option value="other" label="Other">
+//             <div className="demo-option-label-item">Other</div>
+//           </Select.Option>
+//         </Select>
+//       )}
+//     </div>
+//   );
+// }
