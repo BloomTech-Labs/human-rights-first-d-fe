76f6066592b48b840516b0e101b47215e17f89b6
Heath Scott <you@example.com>
2020-11-17 12:09:33 -0600

    🐛 work on getting data to show on display

--- src/state/actions/forceActions.js
+++ src/state/actions/forceActions.js
@@ -1,4 +1,8 @@
-import { ADD_FORCE_DATA, ADD_FORCE_LAYOUT } from '../reducers/force_reducers';
+import {
+  ADD_FORCE_DATA,
+  ADD_FORCE_LAYOUT,
+  ADD_FORCE_USER_INPUT,
+} from '../reducers/force_reducers';
 import axios from 'axios';
 
 export const forceSelection = () => dispatch => {
@@ -8,9 +12,11 @@ export const forceSelection = () => dispatch => {
     .then(res => {
       console.log('forcedata', res.data);
       const force = JSON.parse(res.data);
+      console.log('USER Input', force);
       // dispatches
       dispatch({ type: ADD_FORCE_DATA, payload: force.data });
       dispatch({ type: ADD_FORCE_LAYOUT, payload: force.layout });
+      dispatch({ type: ADD_FORCE_USER_INPUT, payload: force.frames });
     })
     .catch(err => {
       console.log(err);
